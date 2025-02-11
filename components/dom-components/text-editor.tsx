791a8b2ac9b845d9af51615d7327df846632639e
yuri.dunets <yuri.dunets@hiqo-solutions.com>
2025-02-11 17:21:41 +0400

    feat: update app json, add lexical editor

---
+++ components/dom-components/text-editor.tsx
@@ -0,0 +1,79 @@
+"use dom";
+import "./styles.css";
+
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import React from "react";
+
+import ExampleTheme from "./theme";
+import ToolbarPlugin from "./plugins/ToolBarPlugin";
+import TreeViewPlugin from "./plugins/TreeViewPlugin";
+import { $getRoot, EditorState } from "lexical";
+
+const EDITOR_PLACEHOLDER = "Enter some rich text...";
+
+const EDITOR_CONFIG = {
+  namespace: "React.js Demo",
+  nodes: [],
+  onError(error: Error) {
+    throw error;
+  },
+  theme: ExampleTheme,
+};
+
+type DOMProps = import('expo/dom').DOMProps;
+
+interface EditorProps {
+  setPlainText: React.Dispatch<React.SetStateAction<string>>;
+  setEditorState: React.Dispatch<React.SetStateAction<string | null>>;
+  dom?: DOMProps;
+}
+
+export default function Editor({ setPlainText, setEditorState }: EditorProps) {
+  const handleEditorChange = React.useCallback(
+    (editorState: EditorState) => {
+      editorState.read(() => {
+        const root = $getRoot();
+        const textContent = root.getTextContent();
+        setPlainText(textContent);
+      });
+      setEditorState(JSON.stringify(editorState.toJSON()));
+    },
+    [setPlainText, setEditorState]
+  );
+
+  return (
+    <LexicalComposer initialConfig={EDITOR_CONFIG}>
+      <div className="editor-container">
+        <ToolbarPlugin />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className="editor-input"
+                aria-placeholder={EDITOR_PLACEHOLDER}
+                placeholder={
+                  <div className="editor-placeholder">{EDITOR_PLACEHOLDER}</div>
+                }
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <OnChangePlugin
+            onChange={handleEditorChange}
+            ignoreHistoryMergeTagChange
+            ignoreSelectionChange
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          <TreeViewPlugin />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+}
